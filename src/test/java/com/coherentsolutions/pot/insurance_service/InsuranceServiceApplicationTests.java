76071b5bb3bbfa9e95106bbbd42f4ff112da9035
MarkAndz <114013967+MarkAndz@users.noreply.github.com>
2025-07-10 15:34:42 +0300

    Ljt 64 add postgre sql testcontainers support for integration tests (#39)
    * Added Testcontainers dependencies
    
    * Force commons‑compress to 1.27.1 to fix CVEs
    
    * Add PostgreSQL Testcontainer support for integration tests
    
    * (test): added javaagent-test plugin to support inline mock maker
    
    * Added a temporary README.MD

--- src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
+++ src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package com.coherentsolutions.pot.insurance_service;
 
+import com.coherentsolutions.pot.insurance_service.containers.PostgresTestContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InsuranceServiceApplicationTests {
+class InsuranceServiceApplicationTests extends PostgresTestContainer {
 
 	@Test
 	void contextLoads() {
