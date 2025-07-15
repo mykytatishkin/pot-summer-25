6214a45f2813e3c14e8d0fc5e76787f77e42a1c5
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2025-07-15 18:54:17 +0300

    Implement AuditorAware for JPA auditing with Spring Security (#63)
    * AuditAware with dependecies
    
    * implemeting a comment
    
    * bean definition fix
    
    ---------
    
    Co-authored-by: Tishkin <mykyta.tishkin@outlook.com>

--- src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
+++ src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
@@ -3,8 +3,10 @@ package com.coherentsolutions.pot.insurance_service;
 import com.coherentsolutions.pot.insurance_service.containers.PostgresTestContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.main.allow-bean-definition-overriding=true")
 class InsuranceServiceApplicationTests extends PostgresTestContainer {
 
 	@Test
